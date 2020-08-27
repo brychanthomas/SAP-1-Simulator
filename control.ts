69900bf2c54bb9a0b9d8f830a5192b801c3c23b1
unknown <brychan.thomas@btinternet.com>
2020-08-27 10:52:32 +0100

    Added JC and JZ instructions

--- control.ts
+++ control.ts
@@ -1,6 +1,6 @@
 import { Component } from './component.js';
 
-const INSTRUCTION_LOGIC = {
+const MICROCODE = {
     '0000': {}, //NOP
     '0001': { //LDA
       2: ['mi', 'io'],
@@ -26,6 +26,14 @@ const INSTRUCTION_LOGIC = {
     '0110': { //JMP
       2: ['io', 'j']
     },
+    '01110x': {}, //JC when cf = 0
+    '01111x': {   //JC when cf = 1
+      2: ['io', 'j']
+    },
+    '1000x0': {}, //JZ when cf = 0
+    '1000x1': {   //JZ when cf = 1
+      2: ['io', 'j']
+    },
     '1110': { //OUT
       2: ['ao', 'oi']
     },
@@ -48,9 +56,18 @@ export class ControlSequencer extends Component {
       this.computer.controlLines.ci = 1;
     } else {
       var instruction = this.computer.ir.opcode.join('');
+      //if the 4 bit instruction does not exist, try adding the
+      //carry flag register (for conditional jump)
+      if (!MICROCODE.hasOwnProperty(instruction)) {
+        instruction = instruction + this.computer.flagsRegister.cf +'x';
+      }
+      //if cf version of instruction does not exist try zf version
+      if (!MICROCODE.hasOwnProperty(instruction)) {
+        instruction = instruction.slice(0,4) +'x'+this.computer.flagsRegister.zf;
+      }
       //if there are lines to be turned on at this time step
-      if (INSTRUCTION_LOGIC[instruction].hasOwnProperty(this.timeStep)) {
-        var linesToTurnOn = INSTRUCTION_LOGIC[instruction][this.timeStep];
+      if (MICROCODE[instruction].hasOwnProperty(this.timeStep)) {
+        var linesToTurnOn = MICROCODE[instruction][this.timeStep];
         for (var line of linesToTurnOn) {
           this.computer.controlLines[line] = 1;
         }
